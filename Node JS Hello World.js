3b85a2f70844ab323cccd67e20db4559266e75c5
VMac1 <tymacausland@gmail.com>
2021-10-15 14:22:18 -0500

    Updated To Sample Hello World
    Clearer text and added Comments

--- Node JS Hello World.js
+++ Node JS Hello World.js
@@ -1,8 +1,10 @@
+// Standard "Hello World"
+
 'use strict';
 
-console.log('Hello world');
+console.log("Hello world");
 
-console.log('Press any key to continue.');
-process.stdin.once('data', function () {
+console.log("Ctrl+C to exit or type any key and press enter to exit."); // This section waits for an input before continuing so the hello world is readable
+process.stdin.once("data", function () {
     continueProcess();
 });
